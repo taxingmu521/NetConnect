12eb4f7bc220f393f4215292e99f24d494675def
taxingmu <34863021+taxingmu@users.noreply.github.com>
2021-08-15 20:55:30 +0800

    redis

---
+++ Redsi获取多个键/RedisTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using TeamDev.Redis;
+
+namespace Redis.redis
+{
+    /// <summary>
+    /// redis获取所有指定多个键的值
+    /// </summary>
+    public class RedisTask
+    {
+        public string host = "127.0.0.1";
+        public string topic = "__keyspace@0__:*";
+        public int port = 6379;
+        public string passowrd = "task123";
+        private RedisDataAccessProvider redis;
+        List<string> GetVs;
+        public void Connect()
+        {
+            GetVs = new List<string>();
+            redis = new RedisDataAccessProvider();
+            redis.Configuration.Host = host;
+            redis.Configuration.Port = port;
+            var key = "ss";
+            redis.Connect();
+            redis.SendCommand(RedisCommand.KEYS, "*");
+            var keys = redis.ReadMultiString();
+
+            //打印所有的key
+            foreach (var item in keys) {
+                Console.WriteLine(item);
+                GetVs.Add(item);
+            }
+            redis.SendCommand(RedisCommand.MGET, GetVs.ToArray());
+            var kessy = redis.ReadMultiString();
+        }
+    }
+}
